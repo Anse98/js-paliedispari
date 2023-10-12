6a0c2cf9b927aa0e7c1afbeda61814723104234f
Anse98 <lorenzo.anselmi98.com>
2023-10-12 15:11:15 +0200

    ADD-Esercizio Pari o dispari

--- README.md
+++ README.md
@@ -18,3 +18,19 @@
 
   - Stampo il risultato
 
+# PARI O DISPARI
+
+## utente inserisce un numero da 1 a 5 che salvo in una variabile
+
+## creo una piccola funzione che sceglie per il pc un numero random da 1 a 5
+
+  - salvo in una variabile il numero random del pc
+
+## faccio la somma dei due numeri
+
+## creo una funzione che mi restituisce vero se la somma è pari e falso se è dispari
+
+  - SE la somma dei due numeri è pari allora restituisci vero
+
+  - ALTRIMENTI restituisci falso
+## stampo il vincitore
--- app.js
+++ app.js
@@ -30,5 +30,39 @@ if (isPalindrome(userWord)) {
   console.log (userWord + " non è una parola palindroma");
 }
 
+// PARI O DISPARI 
 
+// utente inserisce un numero da 1 a 5 che salvo in una variabile
+const userNum = parseInt(prompt("Scegli pari o dispari e poi inserisci un numero da 1 a 5"));
+console.log("hai scelto " + userNum);
 
+// creo una piccola funzione che sceglie per il pc un numero random da 1 a 5
+function getRandomNum() {
+  return parseInt((Math.random() * (5 - 1) + 1));
+}
+
+// salvo in una variabile il numero random del pc
+const pcNum = getRandomNum();
+console.log ("il pc ha scelto " + pcNum );
+
+// faccio la somma dei due numeri
+const numSum = userNum + pcNum;
+console.log ("la somma dei due numeri è " + numSum);
+
+// creo una funzione che mi restituisce vero se la somma è pari e falso se è dispari
+function isEven() {
+  //  SE la somma dei due numeri è pari allora restituisci vero
+  if (numSum % 2 === 0) {
+    return true;
+  // ALTRIMENTI restituisci falso
+  } else {
+    return false;
+  }
+}
+
+// stampo il vincitore
+if (isEven(numSum)) {
+  console.log("Se hai scelto pari hai vinto! Se hai scelto dispari invece hai perso!")
+} else {
+  console.log("Se hai scelto pari hai perso! Se hai scelto dispari invece hai vinto!")
+}
